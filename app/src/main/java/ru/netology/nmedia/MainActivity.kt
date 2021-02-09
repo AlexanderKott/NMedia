0f08ab4d14e5a9d1df5a2dabb88dc8e20c53a197
Alex Kotikov <alexander.kotikov.mail@gmail.com>
2021-02-09 15:13:00 +0300

    DZ 2.2 mvvm

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,7 +2,10 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewmodels.MainVM
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,33 +13,26 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(999, false, 2000000, 5700)
-        with(binding) {
-            ShareCount.text = digitToString(post.shareCount)
-            likesCount.text = digitToString(post.likesCount)
-            viewsCount.text = digitToString(post.viewsCount)
-            if (post.like) {
-                likeImg.setImageResource(R.drawable.ic_baseline_favorite_24)
-            }
+        val viewModel : MainVM by viewModels()
 
-            likeImg.setOnClickListener {
-                post.like = !post.like
-                likeImg.setImageResource(
-                        if (post.like) R.drawable.ic_baseline_favorite_24
-                        else R.drawable.ic_baseline_favorite_border_24)
-                if (post.like) {
-                    post.likesCount++
-                } else {
-                    post.likesCount--
-                }
-                likesCount.text = digitToString(post.likesCount)
-            }
+   viewModel.data.observe(this,{ post ->
+       with(binding) {
+           ShareCount.text = digitToString(post.shareCount)   //
+           likesCount.text = digitToString(post.likesCount)
+           viewsCount.text = digitToString(post.viewsCount)
+           likeImg.setImageResource(
+               if (post.like) R.drawable.ic_baseline_favorite_24
+               else R.drawable.ic_baseline_favorite_border_24)
+       }
+   })
 
-            shareImg.setOnClickListener {
-                post.shareCount += 1
-                ShareCount.text = digitToString(post.shareCount)
-            }
 
+        binding.likeImg.setOnClickListener {
+            viewModel.like()
+        }
+
+        binding.shareImg.setOnClickListener {
+            viewModel.share()
         }
 
     }
