ec131bc14efc5e74378b98112e50242c9972b8e6
AlexanderKott <root@vmi523325.contaboserver.net>
2021-02-18 10:02:53 +0100

    dz 2.4 fix cancel button

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -70,6 +70,7 @@ class MainActivity : AppCompatActivity() {
                 clearFocus()
                 setText("")
                 hideKeyboard()
+                binding.cancelEditing.isGone = true
             }
 
 
